80abde2aedec47ed0da2f97654d6dc97d8a5c2c7
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-08-19 00:10:44 -0300

    makgin a square using openGL

--- src/Process/CG-Functions.h
+++ src/Process/CG-Functions.h
@@ -5,8 +5,24 @@ extern "C" {
 
 //Callback function called to make the drawing
 void drawing(void) {
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
   //Does clean the visualization window with a background color specified
   glClear(GL_COLOR_BUFFER_BIT);
+
+  //Specifies the current color is RED
+  glColor3f(1.0f, 0.0f, 0.0f);
+
+  //Does drawing a filled square with the current color
+  glBegin(GL_QUADS);
+      glVertex2i(100, 150);
+      glVertex2i(100, 100);
+      //Specifies the current color is blue
+      glColor3f(0.0f, 0.0f, 1.0f);
+      glVertex2i(150,100);
+      glVertex2i(150,150);
+  glEnd();
 
   //Does run the OpenGL commands
   glFlush();
@@ -15,4 +31,25 @@ void drawing(void) {
 void initializes(void) {
   //Define background color of the visualization window with black color
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+//Callback function called when the window size is changed
+void changeSizeWindow(GLsizei w, GLsizei h) {
+  //Prevents the division by zero
+  if(h == 0) {
+    h = 1;
+  }
+  //Specifies the dimensions of the Viewport
+  glViewport(0, 0, w, h);
+  //Initializes the coordinate system
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  //Set the selection window(left, right, bottom, top)
+  if(w <= h) {
+    gluOrtho2D (0.0f, 250.0f, 0.0f, 250.0f*h/w);
+  }
+  else {
+    gluOrtho2D (0.0f, 250.0f*w/h, 0.0f, 250.0f);
+  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+/*
+Author: Thiago Luiz Nunes
+Project: CG-TerrorProject
+*/
 #include "CG-Functions.h"
 #include <iostream>
 
@@ -5,8 +9,12 @@ int main(int argc, char *argv[]) {
 
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(400,350);
+  glutInitWindowPosition(10,10);
   glutCreateWindow("My first progm with OpenGL");
   glutDisplayFunc(drawing);
+  glutReshapeFunc(changeSizeWindow);
+  initializes();
   glutMainLoop();
 
   return 0;
