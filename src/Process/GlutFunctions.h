a611c4c9912b3afd0a4a2d851d04d74d7f0aca63
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-11-27 14:05:58 -0300

    add ilumination

--- src/Process/GlutFunctions.h
+++ src/Process/GlutFunctions.h
@@ -16,7 +16,7 @@ extern "C" {
 float angle = 0.0f;              // angle of rotation for the camera direction
 float lx=0.0f,lz=-1.0f, ly=0.0f; // actual vector representing the camera's direction
 float x=0.0f, z=5.0f;            // XZ position of the camera
-float deltaAngle = 0.0f;         // the key states. These variables will be zero 
+float deltaAngle = 0.0f;         // the key states. These variables will be zero
 float deltaMove = 0;             // when no key is being presses
 float y = 3.0f;
 
@@ -29,7 +29,7 @@ int img_width;
 int img_height;
 int img_channels;
 
-void setObject(std::vector< std::vector<TriangleGL> > objects, 
+void setObject(std::vector< std::vector<TriangleGL> > objects,
 	           std::vector<TextureGL*> textures) {
 
 	_allObjects = objects;
@@ -38,22 +38,14 @@ void setObject(std::vector< std::vector<TriangleGL> > objects,
 	for (int i = 0; i < objects.size(); ++i)
 	{
 		img = textures.at(i)->getData();
-		img_width = textures.at(i)->getWidth(); 
+		img_width = textures.at(i)->getWidth();
 		img_height = textures.at(i)->getHeight();
 		img_channels = textures.at(i)->getChannels();
 
 		glGenTextures( 1, &texture_id );
 		glBindTexture( GL_TEXTURE_2D, texture_id );
 		gluBuild2DMipmaps( GL_TEXTURE_2D, GL_RGB, img_width, img_height, GL_RGBA, GL_UNSIGNED_BYTE, img);
-
-	    // glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // MAG - LINEAR
-		// glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // MINI - MIPMAP
-		
-		// glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-		
 		textures.at(i)->setTextureID(texture_id);
-
-		// glLightModelf(GL_LIGHT_MODEL_COLOR_CONTROL,GL_SEPARATE_SPECULAR_COLOR);
 	}
 }
 void changeSize(int w, int h) {
@@ -63,7 +55,7 @@ void changeSize(int w, int h) {
 		h = 1;
 	}
 	float ratio =  w * 1.0 / h;
-	
+
 	glMatrixMode(GL_PROJECTION);                // Use the Projection Matrix
 	glLoadIdentity();                           // Reset Matrix
 	glViewport(0, 0, w, h);                     // Set the viewport to be the entire window
@@ -90,9 +82,169 @@ void renderScene(void) {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear Color and Depth Buffers
 	glLoadIdentity();                                   // Reset transformations
-	gluLookAt(	x, y , z,																																														
+	gluLookAt(	x, y , z,
 				x+lx, y+ly,  z+lz,
 				0.0f, 1.0f,  0.0f);                     // Set the camera
+
+  //ILUMINAÇÃO GERAL
+  glEnable(GL_COLOR_MATERIAL);
+  glEnable(GL_LIGHTING);//HABILITAR LUZES
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
+  glEnable(GL_LIGHT2);
+  glEnable(GL_LIGHT3);
+  glEnable(GL_LIGHT4);
+  glEnable(GL_LIGHT5);
+  glEnable(GL_LIGHT6);
+  glEnable(GL_LIGHT7);
+  glEnable(GL_NORMALIZE);
+  glEnable(GL_SMOOTH);
+  //glEnable(GL_FLAT);
+
+  GLfloat ambientLight[] = {0.2f, 0.2f, 0.2f, 1.00f};
+  glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
+  glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+  GLfloat specref[] = {1.0f, 1.0f, 1.0f, 1.00f};
+  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,specref);
+  glMateriali(GL_FRONT_AND_BACK,GL_SHININESS,50);
+
+  //atenuação da luzes
+  GLfloat kc = 10;
+
+  glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+
+  // Create light components.
+  //GLfloat ambientLight0[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+  GLfloat diffuseLight0[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+  GLfloat specularLight0[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat position0[] = { 10.0f, 0.0f, 5.2f, 1.0f };
+  GLfloat spot_direction[] = {10.0f, 0.0f, 5.2f};
+
+
+  // Assign created components to GL_LIGHT0
+  //glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight0);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight0);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight0);
+  glLightfv(GL_LIGHT0, GL_POSITION, position0);
+  glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, kc );
+  glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 10.0);
+  glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
+  glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 180.0);
+
+  // Create light components.
+  //GLfloat ambientLight1[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+  GLfloat diffuseLight1[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+  GLfloat specularLight1[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat position1[] = { 13.0f, 13.0f, 5.2f, 1.0f };
+  GLfloat spot_direction1[] = {13.0f, 13.0f, 5.2f};
+
+  // Assign created components to GL_LIGHT1.
+  glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuseLight1);
+  glLightfv(GL_LIGHT1, GL_SPECULAR, specularLight1);
+  glLightfv(GL_LIGHT1, GL_POSITION, position1);
+  glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, kc);
+  glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 10.0);
+  glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spot_direction1);
+  glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 180.0);
+
+  /**************************/
+  // Create light components 2.
+  GLfloat ambientLight2[] = {0.2f, 0.2f, 0.2f, 1.0f };
+  GLfloat diffuseLight2[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+  GLfloat specularLight2[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat position2[] = { 18.0f, 18.0f, 5.2f, 1.0f };
+  GLfloat spot_direction2[] = {18.0f, 18.0f, 1.0f};
+
+  // Assign created components to GL_LIGHT2.
+  glLightfv(GL_LIGHT2, GL_AMBIENT, ambientLight2);
+  glLightfv(GL_LIGHT2, GL_DIFFUSE, diffuseLight2);
+  glLightfv(GL_LIGHT2, GL_SPECULAR, specularLight2);
+  glLightfv(GL_LIGHT2, GL_POSITION, position2);
+  glLightf(GL_LIGHT2, GL_CONSTANT_ATTENUATION, kc);
+  glLightf(GL_LIGHT2, GL_SPOT_EXPONENT, 10.0);
+  glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, spot_direction2);
+  glLightf(GL_LIGHT2, GL_SPOT_CUTOFF, 180.0);
+
+  /**************************/
+  // Create light components 3
+  GLfloat diffuseLight3[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+  GLfloat specularLight3[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat position3[] = { 22.0f, 22.0f, 5.2f, 1.0f };
+  GLfloat spot_direction3[] = {22.0f, 22.0f, 5.2f};
+
+  // Assign created components to GL_LIGHT3.
+  glLightfv(GL_LIGHT3, GL_DIFFUSE, diffuseLight3);
+  glLightfv(GL_LIGHT3, GL_SPECULAR, specularLight3);
+  glLightfv(GL_LIGHT3, GL_POSITION, position3);
+  glLightf(GL_LIGHT3, GL_CONSTANT_ATTENUATION, kc);
+  glLightf(GL_LIGHT3, GL_SPOT_EXPONENT, 10.0);
+  glLightfv(GL_LIGHT3, GL_SPOT_DIRECTION, spot_direction3);
+  glLightf(GL_LIGHT3, GL_SPOT_CUTOFF, 180.0);
+
+  /**************************/
+  // Create light components 4.
+  GLfloat diffuseLight4[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+  GLfloat specularLight4[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat position4[] = { 22.0f, -22.0f, 5.2f, 1.0f };
+  GLfloat spot_direction4[] = {22.0f, -22.0f, 5.2f};
+
+  // Assign created components to GL_LIGHT4.
+  glLightfv(GL_LIGHT4, GL_DIFFUSE, diffuseLight4);
+  glLightfv(GL_LIGHT4, GL_SPECULAR, specularLight4);
+  glLightfv(GL_LIGHT4, GL_POSITION, position4);
+  glLightf(GL_LIGHT4, GL_CONSTANT_ATTENUATION, kc);
+  glLightf(GL_LIGHT4, GL_SPOT_EXPONENT, 10.0);
+  glLightfv(GL_LIGHT4, GL_SPOT_DIRECTION, spot_direction4);
+  glLightf(GL_LIGHT4, GL_SPOT_CUTOFF, 180.0);
+
+  /**************************/
+  // Create light components 5.
+  GLfloat diffuseLight5[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+  GLfloat specularLight5[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat position5[] = { -10.0f, 0.0f, 5.2f, 1.0f };
+  GLfloat spot_direction5[] = {-10.0f, 0.0f, 5.2f};
+
+  // Assign created components to GL_LIGHT5.
+  glLightfv(GL_LIGHT5, GL_DIFFUSE, diffuseLight5);
+  glLightfv(GL_LIGHT5, GL_SPECULAR, specularLight5);
+  glLightfv(GL_LIGHT5, GL_POSITION, position5);
+  glLightf(GL_LIGHT5, GL_CONSTANT_ATTENUATION, kc);
+  glLightf(GL_LIGHT5, GL_SPOT_EXPONENT, 10.0);
+  glLightfv(GL_LIGHT5, GL_SPOT_DIRECTION, spot_direction5);
+  glLightf(GL_LIGHT5, GL_SPOT_CUTOFF, 180.0);
+
+  /**************************/
+  // Create light components 6.
+  GLfloat ambientLight6[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+  GLfloat diffuseLight6[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+  GLfloat specularLight6[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat position6[] = { -13.0f, -13.0f, 5.2f, 1.0f };
+  GLfloat spot_direction6[] = {-13.0f, -13.0f, 5.2f};
+
+  // Assign created components to GL_LIGHT6.
+  glLightfv(GL_LIGHT6, GL_DIFFUSE, diffuseLight6);
+  glLightfv(GL_LIGHT6, GL_SPECULAR, specularLight6);
+  glLightfv(GL_LIGHT6, GL_POSITION, position6);
+  glLightf(GL_LIGHT6, GL_CONSTANT_ATTENUATION, kc);
+  glLightf(GL_LIGHT6, GL_SPOT_EXPONENT, 10.0);
+  glLightfv(GL_LIGHT6, GL_SPOT_DIRECTION, spot_direction6);
+  glLightf(GL_LIGHT6, GL_SPOT_CUTOFF, 180.0);
+
+  /**************************/
+  // Create light components 7.
+  GLfloat diffuseLight7[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+  GLfloat specularLight7[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+  GLfloat position7[] = { -18.0f, -18.0f, 5.2f, 1.0f };
+  GLfloat spot_direction7[] = {-18.0f, -18.0f, 5.2f};
+
+  // Assign created components to GL_LIGHT7
+  glLightfv(GL_LIGHT7, GL_DIFFUSE, diffuseLight7);
+  glLightfv(GL_LIGHT7, GL_SPECULAR, specularLight7);
+  glLightfv(GL_LIGHT7, GL_POSITION, position7);
+  glLightf(GL_LIGHT7, GL_CONSTANT_ATTENUATION, kc);
+  glLightf(GL_LIGHT7, GL_SPOT_EXPONENT, 10.0);
+  glLightfv(GL_LIGHT7, GL_SPOT_DIRECTION, spot_direction7);
+  glLightf(GL_LIGHT7, GL_SPOT_CUTOFF, 180.0);
 
 	DrawGround(); // Draw ground
 
@@ -100,61 +252,55 @@ void renderScene(void) {
 	// glColor3f(0.6f, 0.48f, 0.2f);
 
 	glPushMatrix(); // Draw maze
-		// glTranslatef(0, 1, 0);
 		DrawObject(_allObjects.at(0), _allTextures.at(0));
 	glPopMatrix();
-	
-	glPushMatrix(); // Draw floor																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
-		// glTranslatef(5, 2, -5);
+
+	glPushMatrix(); // Draw floor
 		DrawObject(_allObjects.at(1), _allTextures.at(1));
 	glPopMatrix();
 
-	glPushMatrix(); // Draw ceiling																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
-		// glTranslatef(5, 2, -5);
+	glPushMatrix(); // Draw ceiling
 		DrawObject(_allObjects.at(2), _allTextures.at(2));
 	glPopMatrix();
 
-	glPushMatrix(); // Draw doors																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
-		// glTranslatef(5, 2, -5);
+	glPushMatrix(); // Draw doors
 		DrawObject(_allObjects.at(3), _allTextures.at(3));
 	glPopMatrix();
 
-	glPushMatrix(); // Draw doors lock																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																						
-		// glTranslatef(5, 2, -5);
+	glPushMatrix(); // Draw doors lock
 		DrawObject(_allObjects.at(4), _allTextures.at(4));
 	glPopMatrix();
 
-	glPushMatrix(); // Draw desk																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
+	glPushMatrix(); // Draw desk
 		// glTranslatef(19.2, 0, 17.2);
 		DrawObject(_allObjects.at(5), _allTextures.at(5));
 	glPopMatrix();
 
-	glPushMatrix(); // Draw paintings																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
+	glPushMatrix(); // Draw paintings
 		// glTranslatef(19.2, 0, 18.85);
 		DrawObject(_allObjects.at(6), _allTextures.at(6));
 	glPopMatrix();
 
-	glPushMatrix(); // Draw paintings																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
+	glPushMatrix(); // Draw paintings
 		DrawObject(_allObjects.at(7), _allTextures.at(7));
 	glPopMatrix();
 
-	glPushMatrix(); // Draw lamp																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																							
+	glPushMatrix(); // Draw lamp
 		// DrawObject(_allObjects.at(8), _allTextures.at(8));
 	glPopMatrix();
 
-  	// Draw 36 SnowMen
-  	glPushMatrix();
+	glPushMatrix(); // Draw 36 SnowMen
 		glTranslatef(0, 0, -5);
 		DrawSnowMan();
 	glPopMatrix();
-	
+
 	glFlush();
 	glutSwapBuffers();
 	glutPostRedisplay();
 }
 void initializes (void) {
   // Define background color of the visualization window with black color
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 void pressKey(int key, int xx, int yy) {
 
--- src/Process/GlutManager.cpp
+++ src/Process/GlutManager.cpp
@@ -1,8 +1,8 @@
 #include "GlutManager.hpp"
 #include "GlutFunctions.h"
 
-GlutManager::GlutManager (int *argc, char** argv, int width, int height, const std::string& name, 
-	                      std::vector< std::vector<TriangleGL> > allObjects, 
+GlutManager::GlutManager (int *argc, char** argv, int width, int height, const std::string& name,
+	                      std::vector< std::vector<TriangleGL> > allObjects,
 	                      std::vector<TextureGL*> allTextures) {
 
 	glutInit(argc, argv);
@@ -26,6 +26,41 @@ GlutManager::GlutManager (int *argc, char** argv, int width, int height, const s
 	glutIgnoreKeyRepeat(1); // here are the new entries
 	glutSpecialUpFunc(releaseKey);
 	glEnable(GL_DEPTH_TEST); // OpenGL init
+
+	// Create light components.
+	GLfloat ambientLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+	GLfloat diffuseLight[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	GLfloat specularLight[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat position[] = { 10.0f, 0.0f, 5.2f, 1.0f };
+	//GLfloat spot_direction[] = {0.0f, 0.0f, 0.0f};
+
+	// Assign created components to GL_LIGHT0.
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	//glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 1.0);
+	//glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
+	//glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 180.0);
+
+	glEnable(GL_LIGHT1);
+
+	// Create light components.
+	GLfloat ambientLight1[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+	GLfloat diffuseLight1[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	GLfloat specularLight1[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat position1[] = { 13.0f, 13.0f, 5.2f, 1.0f };
+	//GLfloat spot_direction1[] = {0.0f, 0.0f, 0.0f};
+
+	// Assign created components to GL_LIGHT0.
+	glLightfv(GL_LIGHT1, GL_AMBIENT, ambientLight1);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuseLight1);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, specularLight1);
+	glLightfv(GL_LIGHT1, GL_POSITION, position1);
+	/*glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 1.0);
+	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spot_direction1);
+	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 90.0);*/
+
 	glutMainLoop(); // enter GLUT event processing cycle
 }
 GlutManager::~GlutManager () {}
